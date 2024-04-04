227d44d353ebdf42d1fdedb9ef9877e21d202a33
CamiloD17 <kdodino1999@gmail.com>
2024-04-04 17:55:18 -0500

    Added gallery page

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import BookingListView
+from .views import BookingListView, GalleryListView
 
 app_name="bookings"
 
 
 urlpatterns = [
-  path("", BookingListView.as_view(), name="home")
+  path("", BookingListView.as_view(), name="home"),
+  path("gallery/<str:category>/", GalleryListView.as_view(), name="gallery"),
+  path("gallery/", GalleryListView.as_view(), name="gallery_all"),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
-from .models import Book
+from .models import Book, Category
 
 class BookingListView(View):
   def get(self, request, *args, **kwargs):
@@ -9,3 +9,27 @@ class BookingListView(View):
       'books': books,
     }
     return render(request, "bookings/bookings.html", context)
+
+class GalleryListView(View):
+  def get(self, request, category=None, *args, **kwargs):
+
+    books = Book.objects.all()
+    categories = Category.objects.all()[:6]
+    category_name = None
+
+    if category:
+      books = books.filter(category__name__iexact=category)
+      category_name = Category.objects.get(name__iexact=category).name
+
+    context = {
+      'books': books,
+      'categories': categories,
+      'category_name': category_name
+    }
+
+    template = "bookings/gallery_all.html"
+
+    if category:
+      template = "bookings/gallery.html"
+
+    return render(request, template, context)
